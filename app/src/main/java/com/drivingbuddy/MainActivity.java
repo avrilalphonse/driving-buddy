244bd17ff194ee8b297123a9923324ae891d31f5
Avril Alphonse <avrilalphonse24@gmail.com>
2025-06-29 14:36:57 -0400

    Pages & Nav Bar Setup

--- app/src/main/java/com/drivingbuddy/MainActivity.java
+++ app/src/main/java/com/drivingbuddy/MainActivity.java
@@ -7,18 +7,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.appbar.MaterialToolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find NavHostFragment from the layout
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+        }
+
+        // Find BottomNavigationView from layout
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        // Link BottomNavigationView with NavController
+        if (navController != null && bottomNavigationView != null) {
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        }
     }
 }
