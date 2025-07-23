e3da072a183b9b583b064fae933f68a9a760aa72
Avril Alphonse <avrilalphonse24@gmail.com>
2025-07-23 19:35:46 -0400

    Reorganized files

--- app/src/main/java/com/drivingbuddy/ui/auth/AuthActivity.java
+++ app/src/main/java/com/drivingbuddy/ui/auth/AuthActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.drivingbuddy.R;
-import com.drivingbuddy.ui.LoginFragment;
 
 public class AuthActivity extends AppCompatActivity {
     @Override
