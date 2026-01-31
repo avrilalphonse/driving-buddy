660b9193e6cbed15de3eabf710600c973bcaa5ea
Maya Shenoy <m2shenoy@uwaterloo.ca>
2026-01-31 18:37:45 -0500

    add endpoint to fetch data from persistent_summary_data collection

--- app/src/main/java/com/drivingbuddy/data/api/SensorDataApiService.java
+++ app/src/main/java/com/drivingbuddy/data/api/SensorDataApiService.java
@@ -9,4 +9,7 @@ public interface SensorDataApiService {
 
     @GET("api/sensor-data/get-bucketed-data")
     Call<BucketedDataResponse> getBucketedData(@Query("windowSeconds") int windowSeconds);
+
+    @GET("api/sensor-data/get-persistent-summary")
+    Call<BucketedDataResponse> getPersistentSummaryData();
 }
